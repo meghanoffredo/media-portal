0f80dd0a0c300a0430724386c4aeb32a358599a1
Meghan Offredo <meghan.offredo@gmail.com>
2021-12-16 14:26:14 +0400

    shift back to local storage (vs session storage)

--- src/Components/UserName.jsx
+++ src/Components/UserName.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 // remove double & single quotes from string
-const getName = sessionStorage.getItem('name');
+const getName = localStorage.getItem('name');
 const withoutQuotes = getName.replace(/['"]+/g, '');
 export default class UserName extends React.Component {
 
